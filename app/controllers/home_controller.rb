7c46c42826ef7b6e210d30da320ee14833af230e
John Siladie <john.siladie@gmail.com>
2017-04-04 23:58:17 -0600

    Search page showing raw JSON results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-    
+    search_term = params[:q]
+
+    if search_term.present?
+      @search_term = search_term
+      @search_results = OpenMovieDatabase.movie_search(search_term)
+    end
   end
 end
