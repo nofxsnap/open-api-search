3ac4350416fc4f33a1e85ccdecee0bfabefe8380
John Siladie <john.siladie@gmail.com>
2017-04-05 01:18:40 -0600

    Truncate long search terms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
     search_term = params[:q]
 
     if search_term.present?
+      # Truncate the search term if someone is trying to be silly
+      search_term = search_term[0..250]
       @search_term = search_term
       @search_results = OpenMovieDatabase.movie_search(@search_term)[:Search]
       MovieSearch.add_or_increment(@search_term) if @search_results.present?
