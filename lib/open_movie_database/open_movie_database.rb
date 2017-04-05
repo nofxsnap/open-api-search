bce1d427c105889a24b2e6426d0a5502ab0a74f2
John Siladie <john.siladie@gmail.com>
2017-04-05 00:33:20 -0600

    Made a left vs right side to have a current search and a past search area

--- lib/open_movie_database/open_movie_database.rb
+++ lib/open_movie_database/open_movie_database.rb
@@ -7,17 +7,25 @@ class OpenMovieDatabase
     movie_query = "http://www.omdbapi.com/?s=#{movie}&type=movie&page=1"
     response = RestClient.get(movie_query)
 
+    data = nil
     # If the response is valid then parse it into JSON
     if response.present? && good_status?(response.code)
-      JSON.parse(response.body) if response.body.present?
-    else
-      nil
+      data = JSON.parse(response.body) if response.body.present?
     end
+    convert_to_mash(data)
   end
 
   private
 
   def self.good_status?(response_code)
     response_code == 200 || response_code == 201 || response_code == 204
+  end
+
+  def self.convert_to_mash(data)
+    if data.is_a? Hash
+      Hashie::Mash.new(data)
+    elsif data.is_a? Array
+      data.map { |d| Hashie::Mash.new(d) }
+    end
   end
 end
