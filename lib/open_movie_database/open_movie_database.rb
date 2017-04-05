e5d812ec69a99b4d4fa8b4940416d69aa4023553
John Siladie <john.siladie@gmail.com>
2017-04-04 23:34:48 -0600

    Encode whitespace into %20 and added some comments

--- lib/open_movie_database/open_movie_database.rb
+++ lib/open_movie_database/open_movie_database.rb
@@ -1,6 +1,13 @@
 class OpenMovieDatabase
   def self.movie_search(movie)
-    response = RestClient.get "http://www.omdbapi.com/?s=#{movie}&type=movie&page=1"
+    # Encode whitespace with %20
+    movie = URI.escape(movie)
+
+    # Just return the first page as it gets really slow if you search for something very generic
+    movie_query = "http://www.omdbapi.com/?s=#{movie}&type=movie&page=1"
+    response = RestClient.get(movie_query)
+
+    # If the response is valid then parse it into JSON
     if response.present? && good_status?(response.code)
       JSON.parse(response.body) if response.body.present?
     else
